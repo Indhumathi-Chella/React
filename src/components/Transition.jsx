3f599c08ef300b8f81c3777374e7e0996a7ad386
indhumathi.chella@aspiresys.com <indhumathi.chella@aspiresys.com>
2025-06-25 17:48:42 +0530

    update:transition hook

--- src/components/Transition.jsx
+++ src/components/Transition.jsx
@@ -1,31 +1,75 @@
-import { useState, useTransition } from 'react';
+import { useState, useTransition } from "react";
 
-export default function SearchList({ items }) {
-  const [input, setInput] = useState('');
-  const [filteredItems, setFilteredItems] = useState(items);
+function SearchList({ items }) {
+  const [input, setInput] = useState("");
   const [isPending, startTransition] = useTransition();
+  const [result, setResult] = useState(items);
 
   function handleChange(e) {
     const value = e.target.value;
     setInput(value);
 
     startTransition(() => {
-      const filtered = items.filter(item =>
+      const filtered = items.filter((item) =>
         item.toLowerCase().includes(value.toLowerCase())
       );
-      setFilteredItems(filtered);
+      setResult(filtered);
+    });
+  }
+  return (
+    <>
+      <input type="text" value={input} onChange={handleChange} />
+      {isPending && <p>Loading...</p>}
+      {result.length === 0 ? "Not found" : " "}
+
+      <ul>
+        {result.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+
+function SearchLists({ items }) {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState(items);
+  const [isPending, startTransition] = useTransition();
+  const [isLoading, setIsLoading] = useState(false); 
+
+  async function handleChange(e) {
+    const value = e.target.value;
+    setInput(value);
+    setIsLoading(true); 
+    const filtered = await new Promise((resolve) => {
+      setTimeout(() => {
+        const data = items.filter(item =>
+          item.toLowerCase().includes(value.toLowerCase())
+        );
+        resolve(data);
+      }, 500); 
+    });
+
+    startTransition(() => {
+      setResult(filtered);
+      setIsLoading(false);
     });
   }
 
   return (
-    <div>
+    <>
       <input type="text" value={input} onChange={handleChange} />
-      {isPending && <p>Loading...</p>}
+      {(isPending || isLoading) && <p>Loading...</p>}
       <ul>
-        {filteredItems.map((item, i) => (
-          <li key={i}>{item}</li>
+        {result.map(item => (
+          <li key={item}>{item}</li>
         ))}
       </ul>
-    </div>
+    </>
   );
 }
+
+
+
+export { SearchList,SearchLists };
